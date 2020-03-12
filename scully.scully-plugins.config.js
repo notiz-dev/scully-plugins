926f62d4b3cea5f4299984ab575d9b82af28413e
Marc Stammerjohann <marcstammerjohann@web.de>
2020-03-12 13:07:54 +0100

    feat(scully): add plugins to post renderes

--- scully.scully-plugins.config.js
+++ scully.scully-plugins.config.js
@@ -1,7 +1,10 @@
+require('./plugins/lazy-images');
+require('./plugins/fouc');
+
 exports.config = {
-  projectRoot: "./src",
-  projectName: "scully-plugins",
+  projectRoot: './src',
+  projectName: 'scully-plugins',
+  defaultPostRenderers: ['lazyImages', 'fouc'],
   outDir: './dist/static',
-  routes: {
-  }
+  routes: {}
 };
