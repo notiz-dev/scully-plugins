d56e9dbe0e8dcdfaae9a5e50afc831f8225b2e7a
garygrossgarten <gary@fivethree.io>
2020-03-13 15:35:56 +0100

    update rss plugin

--- plugins/rss/src/rss.js
+++ plugins/rss/src/rss.js
@@ -1,28 +1,42 @@
-const { scullyConfig } = require('@scullyio/scully/utils/config');
-const RSS = require('rss');
-const { writeFile } = require('fs');
-const { promisify } = require('util');
+const Feed = require('feed').Feed;
+const { writeFileSync, readFileSync } = require('fs');
 const { join } = require('path');
-const writeFileAsync = promisify(writeFile);
-const config = scullyConfig.rssConfig || {
-  title: 'RSS Feed',
-  site_url: 'https://example.com',
-  feed_url: 'https://example.com/feed'
-};
-
-const feed = new RSS(config);
+let config;
+let feed;
 const rssPlugin = async (html, route) => {
+  try {
+    if (!config) {
+      const configFile = readFileSync(
+        `${process.cwd()}/rss.config.json`,
+        'utf8'
+      );
+      config = JSON.parse(configFile.toString());
+      feed = new Feed(config);
+      config.categories.forEach(cat => {
+        feed.addCategory(cat);
+      });
+    }
+    const item = {
+      title: route.data.title,
+      id: route.route,
+      link: route.route,
+      description: route.data.description,
+      // content: post.content,
+      author: route.data.authors.map(a => ({ name: a })),
+      contributor: route.data.authors.map(a => ({
+        name: a.toLowerCase().replace(' ', '-')
+      })),
+      date: route.data.updatedAt || route.data.publishedAt,
+      image: route.data.twitterBanner
+    };
+    feed.addItem(item);
+    writeFileSync(join(config.outDir || '', 'feed.xml'), feed.rss2());
+    writeFileSync(join(config.outDir || '', 'feed.atom'), feed.atom1());
+    writeFileSync(join(config.outDir || '', 'feed.json'), feed.json1());
+  } catch (err) {
+    console.error(err);
+  }
 
-  const item = {
-    date: route.data.updatedAt || route.data.publishedAt,
-    description: route.data.description || '',
-    title: route.data.title || '',
-    url: `${config.site_url}/${route.route}`,
-    categories: route.data.categories || route.data.tags,
-    author: route.data.author || route.data.authors[0]
-  };
-  feed.item(item);
-  await writeFileAsync(join(scullyConfig.outDir || '', 'feed.xml'), feed.xml());
   return html;
 };
 
