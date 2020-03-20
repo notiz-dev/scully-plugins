cb3517c129c5de4ee316d93a1732910c1e39ce3b
Marc Stammerjohann <marcstammerjohann@web.de>
2020-03-20 20:09:14 +0100

    add random pictures and mediumZoom plugin
    * random pictures are not loading with lazyImages

--- scully.scully-plugins.config.js
+++ scully.scully-plugins.config.js
@@ -1,17 +1,19 @@
 require('./plugins/lazy-images');
 require('./plugins/fouc');
+require('./plugins/medium-zoom');
 
 exports.config = {
   projectRoot: './src',
   projectName: 'scully-plugins',
-  defaultPostRenderers: ['lazyImages', 'fouc'],
+  defaultPostRenderers: ['fouc'],
   outDir: './dist/static',
   routes: {
     '/blog/:slug': {
       type: 'contentFolder',
       slug: {
         folder: './content/blog'
-      }
+      },
+      postRenderers: ['fouc', 'mediumZoom']
     }
   }
 };
