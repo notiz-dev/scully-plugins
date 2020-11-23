766cf9f7455841372430921777a768f1391accd4
Marc Stammerjohann <marcstammerjohann@web.de>
2020-11-23 09:06:51 +0100

    bump to angular 11

--- src/app/components/seo/seo.component.spec.ts
+++ src/app/components/seo/seo.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { SeoComponent } from './seo.component';
 
@@ -6,7 +6,7 @@ describe('SeoComponent', () => {
   let component: SeoComponent;
   let fixture: ComponentFixture<SeoComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ SeoComponent ]
     })
--- src/app/doc/doc.component.spec.ts
+++ src/app/doc/doc.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { DocComponent } from './doc.component';
 
@@ -6,7 +6,7 @@ describe('DocComponent', () => {
   let component: DocComponent;
   let fixture: ComponentFixture<DocComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ DocComponent ]
     })
